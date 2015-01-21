04e665a499174b0c601b9e1f0951fbe1883f80e3
klliu <klliu@lewatek.com>
2015-01-21 20:03:39 +0800

    Title: Implement the ShaderUtil class

--- app/src/main/java/gles/alex/com/opengles/MainActivity.java
+++ app/src/main/java/gles/alex/com/opengles/MainActivity.java
@@ -4,14 +4,27 @@ import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.TextView;
 
 
 public class MainActivity extends ActionBarActivity {
 
+    TextView textView = null;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        textView = (TextView) findViewById(R.id.text_view);
+
+        textView.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+
     }
 
 
